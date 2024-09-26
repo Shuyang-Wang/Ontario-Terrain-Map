87a98978b0a9ed8a6c6e2a456a95136f28c93eab
Shuyang-Wang <sawyer.wang@hotmail.com>
2024-09-26 12:57:23 -0400

    update

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
 import GeoTIFF from 'https://cdn.jsdelivr.net/npm/ol/source/GeoTIFF.js';
 import Map from 'https://cdn.jsdelivr.net/npm/ol/Map.js';
-import TileLayer from 'https://cdn.jsdelivr.net/npm/ol/layer/WebGLTile.js';
+import WebGLTile from 'https://cdn.jsdelivr.net/npm/ol/layer/WebGLTile.js';
 
-// URL of your Cloud Optimized GeoTIFF (COG)
 const cogUrl = 'https://ccemp-bucket.s3.us-east-2.amazonaws.com/Public+Access/COG/merged_output_cog.tif';
 
 // Create the GeoTIFF source
 const source = new GeoTIFF({
-    sources: [
-        {
-            url: cogUrl,
-        },
-    ],
+  sources: [
+    {
+      url: cogUrl,
+    },
+  ],
 });
 
 // Initialize the map
 const map = new Map({
-    target: 'map',
-    layers: [
-        new TileLayer({
-            source: source,
-        }),
-    ],
-    view: source.getView(),
+  target: 'map',
+  layers: [
+    new WebGLTile({
+      source: source,
+    }),
+  ],
+  view: source.getView(),
 });
